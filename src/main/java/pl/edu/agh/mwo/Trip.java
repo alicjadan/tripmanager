fead975bc17ad6956876d03af6f0c6beb07fe2f4
student6 <student6@pcoip.ki.agh.edu.pl>
2018-03-17 12:18:37 +0100

    firs commit

---
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -0,0 +1,21 @@
+package pl.edu.agh.mwo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Trip {
+
+	public String name;
+	public String description;
+	
+	Collection <Photo> photos = new ArrayList<Photo>();
+
+	public Collection<Photo> getPhotos() {
+		return photos;
+	}
+
+	public void addPhoto(Photo photo) {
+		photos.add(photo);
+	}
+
+}
